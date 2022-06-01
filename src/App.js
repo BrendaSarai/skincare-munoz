3f018ee666958b0c85114e8218b2ba1dfdf691e5
Brenda Sarai Muñoz <78765955+BrendaSarai@users.noreply.github.com>
2022-05-31 20:23:39 -0500

    Personalización

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
+import logo from './icono.webp';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <h1>Brenda Sarai Muñoz Pluma</h1>
+      <h1>SkinCare</h1>
+      <header className="">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
-    </div>
+    <p>Este sitio contendrá información relevante acerca de los cuidados de la piel.</p>
+  </div>
   );
 }
 
